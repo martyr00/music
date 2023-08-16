9f448263b4723355ee2aebcc5293afff25bca55d
martyr00 <leonyvb04@gmail.com>
2023-08-16 13:02:20 +0300

    migrations

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'music.apps.MusicConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,11 +78,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'martyr',
+        'USER': 'martyr',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '6543',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- music/models.py
+++ music/models.py
@@ -1,3 +1,64 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+def album_upload_path(instance, filename):
+    author = instance.author.username
+    title_album = instance.album
+
+    upload_path = os.path.join('', str(author), str(title_album), filename)
+    return f"music/{upload_path}"
+
+
+def music_upload_path(instance, filename):
+    title_music = instance.title
+    title_album_music = instance.album.title
+    user = instance.author.username
+
+    upload_path = os.path.join('', str(user), str(title_album_music), str(title_music), filename)
+    return f"music/{upload_path}"
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+
+    image = models.ImageField(upload_to=album_upload_path)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='albums_authored')
+    on_feat = models.ManyToManyField(User, related_name='albums_featured')
+    genre = models.ManyToManyField(Genre, related_name='album_genre')
+
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='created_time')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='update_time')
+
+
+class Music(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to=music_upload_path)
+    image = models.ImageField(upload_to=music_upload_path)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='music_authored')
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='album_music')
+
+    on_feat = models.ManyToManyField(User, related_name='music_featured')
+    in_fav = models.ManyToManyField(User, related_name='music_favorites')
+    genre = models.ManyToManyField(Genre, related_name='genre')
+
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='created_time')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='update_time')
+
+
+class PlayList(models.Model):
+    title = models.CharField(max_length=100)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='play_list_authored')
+    music = models.ManyToManyField(Music, related_name='music_play_list')
+
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='created_time')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='update_time')
